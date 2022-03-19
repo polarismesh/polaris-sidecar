8eba835dd700f9505a0dc14e3565dacb0e77ccfe
springliao <springliao@tencent.com>
2022-03-19 13:22:26 +0800

    refactor: 调整VM安装包名称

--- build_auto.sh
+++ build_auto.sh
@@ -7,7 +7,26 @@ bash build.sh
 
 # 构建安装包
 
-package_name="polaris-sidecar-install-${version}.zip"
+set -e
+
+if [ $# -gt 0 ]; then
+  version="$1"
+else
+  current=`date "+%Y-%m-%d %H:%M:%S"`
+  timeStamp=`date -d "$current" +%s`
+  currentTimeStamp=$(((timeStamp*1000+10#`date "+%N"`/1000000)/1000))
+  version="$currentTimeStamp"
+fi
+workdir=$(dirname $(realpath $0))
+bin_name="polaris-sidecar"
+if [ "${GOOS}" == "" ]; then
+  GOOS=$(go env GOOS)
+fi
+if [ "${GOARCH}" == "" ]; then
+  GOARCH=$(go env GOARCH)
+fi
+
+package_name="polaris-sidecar-install_${version}.${GOOS}.${GOARCH}.zip"
 folder_name="polaris-sidecar-install"
 
 mkdir -p ${folder_name}
